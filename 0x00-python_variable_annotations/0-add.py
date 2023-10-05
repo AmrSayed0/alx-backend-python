b65b3e4544c0ada37d3a42ec54a636532f3a69de
AmrSayed0 <amracademycode@gmail.com>
2023-10-05 19:02:02 +0300

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains the function for task 0"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats and returns the sum."""
+    return a + b
